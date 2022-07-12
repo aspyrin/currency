7447b17167104f718f31707cc274f0cad0b80827
Andrii Bobrov <aspyrin77@gmail.com>
2022-07-12 14:03:09 +0300

    add comment in def hello_world

--- app/currency/views.py
+++ app/currency/views.py
@@ -4,6 +4,15 @@ from django.http import HttpResponse
 
 
 def hello_world(request):
+    """
+    Test function
+    :param
+    request: name
+    :return:
+    if no get param -> Hello, World!
+    if get param 'name' -> Hello, <Name>!
+    """
+
     name = request.GET.get('name')
     if not request.GET.get('name'):
         result = 'Hello, World!'
