5087ff03c8f02ca7f8134d7208d70789cc9d0e92
Andrii Bobrov <aspyrin77@gmail.com>
2022-07-18 03:07:47 +0300

    add models Rate, ContactUs

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,3 +1,18 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Rate(models.Model):
+    base_currency_type = models.CharField(max_length=3)
+    currency_type = models.CharField(max_length=3)
+    sale = models.DecimalField(max_digits=10, decimal_places=4)
+    buy = models.DecimalField(max_digits=10, decimal_places=4)
+    source = models.CharField(max_length=64)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class ContactUs(models.Model):
+    email_from = models.EmailField(max_length=30)
+    email_to = models.EmailField(max_length=30)
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+    sended = models.DateTimeField(auto_now_add=True)
