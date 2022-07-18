f0fe9d0459224b09f861993f16e407a59aadde0b
Andrii Bobrov <aspyrin77@gmail.com>
2022-07-18 03:13:16 +0300

    add urlpatterns rate_generator/, rate_list/, contactus_generator/, contactus_list/

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from currency.views import hello_world
+from currency.views import rate_generator, rate_list, contactus_list, contactus_generator
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('hello_world/', hello_world),
+    path('rate_generator/', rate_generator),
+    path('rate_list/', rate_list),
+    path('contactus_generator', contactus_generator),
+    path('contactus_list/', contactus_list),
 ]
