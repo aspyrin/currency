9cf9e83b22a19366a6f236ca8fea2892cb5dfef1
Andrii Bobrov <aspyrin77@gmail.com>
2022-07-24 16:56:34 +0300

    create model forms: RateForm, ContactUsForm, SourceForm

---
+++ app/currency/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from currency.models import Rate, ContactUs, Source
+
+
+class RateForm(forms.ModelForm):
+    class Meta:
+        model = Rate
+        fields = (
+            'base_currency_type',
+            'currency_type',
+            'sale',
+            'buy',
+            'source',
+        )
+
+
+class ContactUsForm(forms.ModelForm):
+    class Meta:
+        model = ContactUs
+        fields = (
+            'email_from',
+            'email_to',
+            'subject',
+            'message',
+        )
+        widgets = {
+            'subject': forms.Textarea(attrs={'cols': 100, 'rows': 2}),
+            'message': forms.Textarea(attrs={'cols': 100, 'rows': 5}),
+        }
+
+
+class SourceForm(forms.ModelForm):
+    class Meta:
+        model = Source
+        fields = (
+            'source_url',
+            'name',
+        )
