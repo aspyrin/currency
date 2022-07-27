9cc19660a0f70cffee9b7d4739d97a5c2780b9b4
Andrii Bobrov <aspyrin77@gmail.com>
2022-07-27 17:44:22 +0300

    change controllers on classes based views and fix templates

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from currency.models import Rate, ContactUs, Source
-from currency.utils import rates_gen, contactus_gen
+from currency import utils
+
 from currency.forms import RateForm, ContactUsForm, SourceForm
+from django.views import generic
 
 
-def index(request):
-    """
-    :param
-    none
-    :return:
-    link to home page
-    """
-    context = {
-        'title': 'Home page',
-        'message': 'Currency exchange project',
-    }
-    return render(request, 'index.html', context=context)
+class IndexView(generic.TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Currency Exchange Project - Home page'
+        last_rate_date = utils.get_last_rate_date()
+        context['last_rate_date'] = last_rate_date
+        context['currency_types_list'] = utils.get_currency_types()
+        context['sources_list'] = utils.get_sources()
+        context['last_rate_list'] = utils.get_last_rate_list(last_rate_date)
+        return context
 
 
 def rate_generator(request):
@@ -27,200 +29,8 @@ def rate_generator(request):
     :return:
     link to rate_list
     """
-    rates_gen()
+    utils.rates_gen()
     return redirect('/rate_list/')
-
-
-def source_list(request):
-    """
-    function return all records of Source model
-    :param
-    none
-    :return:
-    all records of Source model
-    """
-    context = {
-        'title': 'Source list',
-        'source_list': Source.objects.all(),
-    }
-    return render(request, 'source_list.html', context=context)
-
-
-def source_create(request):
-    """
-    function create object of Source model
-    :param
-    none
-    :return:
-    redirect to /source_list/
-    """
-    if request.method == 'POST':
-        form = SourceForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/source_list/')
-    elif request.method == 'GET':
-        form = SourceForm()
-    context = {
-        'title': 'Source create',
-        'form': form,
-    }
-    return render(request, 'source_create.html', context=context)
-
-
-def source_update(request, source_id):
-    """
-    function update object of Source model by id
-    :param
-    none
-    :return:
-    redirect to /source_list/
-    """
-    source_instance = get_object_or_404(Source, id=source_id)
-
-    if request.method == 'POST':
-        form = SourceForm(request.POST, instance=source_instance)
-        if form.is_valid():
-            form.save()
-            return redirect('/source_list/')
-    elif request.method == 'GET':
-        form = SourceForm(instance=source_instance)
-    context = {
-        'title': 'Source update',
-        'form': form,
-    }
-    return render(request, 'source_update.html', context=context)
-
-
-def source_details(request, source_id):
-    """
-    function return one object of Source model by id
-    :param
-    none
-    :return:
-    one object of Source model by id
-    """
-    source_instance = get_object_or_404(Source, id=source_id)
-    context = {
-        'title': 'Source details',
-        'instance': source_instance,
-    }
-    return render(request, 'source_details.html', context=context)
-
-
-def source_delete(request, source_id):
-    """
-    function delete object of Source model by id
-    :param
-    none
-    :return:
-    redirect to /source_list/
-    """
-    source_instance = get_object_or_404(Source, id=source_id)
-    if request.method == 'POST':
-        source_instance.delete()
-        return redirect('/source_list/')
-    context = {
-        'title': 'Source delete',
-        'instance': source_instance,
-    }
-    return render(request, 'source_delete.html', context=context)
-
-
-def rate_list(request):
-    """
-    function return all records of Rate model
-    :param
-    none
-    :return:
-    all records of Rate model
-    """
-    context = {
-        'title': 'Rate list',
-        'rate_list': Rate.objects.all(),
-    }
-    return render(request, 'rate_list.html', context=context)
-
-
-def rate_create(request):
-    """
-    function create object of Rate model
-    :param
-    none
-    :return:
-    redirect to /rate_list/
-    """
-    if request.method == 'POST':
-        form = RateForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/rate_list/')
-    elif request.method == 'GET':
-        form = RateForm(initial={'base_currency_type': 'UAN'})
-    context = {
-        'title': 'Rate create',
-        'form': form,
-    }
-    return render(request, 'rate_create.html', context=context)
-
-
-def rate_update(request, rate_id):
-    """
-    function update object of Rate model by id
-    :param
-    none
-    :return:
-    redirect to /rate_list/
-    """
-    rate_instance = get_object_or_404(Rate, id=rate_id)
-
-    if request.method == 'POST':
-        form = RateForm(request.POST, instance=rate_instance)
-        if form.is_valid():
-            form.save()
-            return redirect('/rate_list/')
-    elif request.method == 'GET':
-        form = RateForm(instance=rate_instance)
-    context = {
-        'title': 'Rate update',
-        'form': form,
-    }
-    return render(request, 'rate_update.html', context=context)
-
-
-def rate_details(request, rate_id):
-    """
-    function return one object of Rate model by id
-    :param
-    none
-    :return:
-    one object of Rate model by id
-    """
-    rate_instance = get_object_or_404(Rate, id=rate_id)
-    context = {
-        'title': 'Rate details',
-        'instance': rate_instance,
-    }
-    return render(request, 'rate_details.html', context=context)
-
-
-def rate_delete(request, rate_id):
-    """
-    function delete object of Rate model by id
-    :param
-    none
-    :return:
-    redirect to /rate_list/
-    """
-    rate_instance = get_object_or_404(Rate, id=rate_id)
-    if request.method == 'POST':
-        rate_instance.delete()
-        return redirect('/rate_list/')
-    context = {
-        'title': 'Rate delete',
-        'instance': rate_instance,
-    }
-    return render(request, 'rate_delete.html', context=context)
 
 
 def contactus_generator(request):
@@ -232,101 +42,173 @@ def contactus_generator(request):
     :return:
     link to contactus_list
     """
-    contactus_gen()
+    utils.contactus_gen()
     return redirect('/contactus_list/')
 
 
-def contactus_list(request):
-    """
-    function show all records of model ContactUs
-    :param
-    no
-    :return:
-    all records of model ContactUs
-    """
-    context = {
-        'title': 'Contact Us list',
-        'contactus_list': ContactUs.objects.all(),
-    }
-    return render(request, 'contactus_list.html', context=context)
+# =================Source==================
+class SourceListView(generic.ListView):
+    queryset = Source.objects.all()
+    template_name = 'source_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Source list'
+        return context
 
 
-def contactus_create(request):
-    """
-    function create object of ContactUs model
-    :param
-    none
-    :return:
-    redirect to /contactus_list/
-    """
-    if request.method == 'POST':
-        form = ContactUsForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/contactus_list/')
-    elif request.method == 'GET':
-        form = ContactUsForm(initial={'email_from': 'curency@gmail.com'})
-    context = {
-        'title': 'ContactUs create',
-        'form': form,
-    }
-    return render(request, 'contactus_create.html', context=context)
+class SourceCreateView(generic.CreateView):
+    queryset = Source.objects.all()
+    template_name = 'source_create.html'
+    form_class = RateForm
+    success_url = '/source_list/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Source create'
+        return context
 
 
-def contactus_details(request, contactus_id):
-    """
-    function return one object of ContactUs model by id
-    :param
-    none
-    :return:
-    one object of ContactUs model by id
-    """
-    contactus_instance = get_object_or_404(ContactUs, id=contactus_id)
-    context = {
-        'title': 'ContactUs details',
-        'instance': contactus_instance,
-    }
-    return render(request, 'contactus_details.html', context=context)
+class SourceUpdateView(generic.UpdateView):
+    queryset = Source.objects.all()
+    template_name = 'source_update.html'
+    form_class = RateForm
+    success_url = '/source_list/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Source update'
+        return context
 
 
-def contactus_delete(request, contactus_id):
-    """
-    function delete object of ContactUs model by id
-    :param
-    none
-    :return:
-    redirect to /contactus_list/
-    """
-    contactus_instance = get_object_or_404(ContactUs, id=contactus_id)
-    if request.method == 'POST':
-        contactus_instance.delete()
-        return redirect('/contactus_list/')
-    context = {
-        'title': 'ContactUs delete',
-        'instance': contactus_instance,
-    }
-    return render(request, 'contactus_delete.html', context=context)
+class SourceDeleteView(generic.DeleteView):
+    queryset = Source.objects.all()
+    template_name = 'source_delete.html'
+    success_url = '/source_list/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Source delete'
+        return context
 
 
-def contactus_update(request, contactus_id):
-    """
-    function update object of ContactUs model by id
-    :param
-    none
-    :return:
-    redirect to /contactus_list/
-    """
-    contactus_instance = get_object_or_404(ContactUs, id=contactus_id)
+class SourceDetailsView(generic.DetailView):
+    queryset = Source.objects.all()
+    template_name = 'source_details.html'
 
-    if request.method == 'POST':
-        form = ContactUsForm(request.POST, instance=contactus_instance)
-        if form.is_valid():
-            form.save()
-            return redirect('/contactus_list/')
-    elif request.method == 'GET':
-        form = ContactUsForm(instance=contactus_instance)
-    context = {
-        'title': 'ContactUs update',
-        'form': form,
-    }
-    return render(request, 'contactus_update.html', context=context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Source details'
+        return context
+
+
+# =================Rate===================
+class RateListView(generic.ListView):
+    queryset = Rate.objects.all()
+    template_name = 'rate_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Rate list'
+        return context
+
+
+class RateCreateView(generic.CreateView):
+    queryset = Rate.objects.all()
+    template_name = 'rate_create.html'
+    form_class = RateForm
+    success_url = '/rate_list/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Rate create'
+        return context
+
+
+class RateUpdateView(generic.UpdateView):
+    queryset = Rate.objects.all()
+    template_name = 'rate_update.html'
+    form_class = RateForm
+    success_url = '/rate_list/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Rate update'
+        return context
+
+
+class RateDeleteView(generic.DeleteView):
+    queryset = Rate.objects.all()
+    template_name = 'rate_delete.html'
+    success_url = '/rate_list/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Rate delete'
+        return context
+
+
+class RateDetailsView(generic.DetailView):
+    queryset = Rate.objects.all()
+    template_name = 'rate_details.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Rate details'
+        return context
+
+
+# =================ContactUs===================
+class ContactUsListView(generic.ListView):
+    queryset = ContactUs.objects.all()
+    template_name = 'contactus_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Contact Us list'
+        return context
+
+
+class ContactUsCreateView(generic.CreateView):
+    queryset = ContactUs.objects.all()
+    template_name = 'contactus_create.html'
+    form_class = RateForm
+    success_url = '/contactus_list/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Contact Us create'
+        return context
+
+
+class ContactUsUpdateView(generic.UpdateView):
+    queryset = ContactUs.objects.all()
+    template_name = 'contactus_update.html'
+    form_class = RateForm
+    success_url = '/contactus_list/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Contact Us update'
+        return context
+
+
+class ContactUsDeleteView(generic.DeleteView):
+    queryset = ContactUs.objects.all()
+    template_name = 'contactus_delete.html'
+    success_url = '/contactus_list/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Contact Us delete'
+        return context
+
+
+class ContactUsDetailsView(generic.DetailView):
+    queryset = ContactUs.objects.all()
+    template_name = 'contactus_details.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Contact Us details'
+        return context
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,37 +1,35 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-from currency.views import index
 from currency.views import rate_generator
 from currency.views import contactus_generator
-from currency.views import source_list, source_create, source_update, source_details, source_delete
-from currency.views import rate_list, rate_create, rate_update, rate_details, rate_delete
-from currency.views import contactus_create, contactus_list, contactus_details, contactus_update, contactus_delete
-
+from currency import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', index),
+    path('', views.IndexView.as_view()),
 
     path('rate_generator/', rate_generator),
-
-    path('source_list/', source_list),
-    path('source_create/', source_create),
-    path('source_update/<int:source_id>', source_update),
-    path('source_details/<int:source_id>', source_details),
-    path('source_delete/<int:source_id>', source_delete),
-
-    path('rate_list/', rate_list),
-    path('rate_create/', rate_create),
-    path('rate_update/<int:rate_id>', rate_update),
-    path('rate_details/<int:rate_id>', rate_details),
-    path('rate_delete/<int:rate_id>', rate_delete),
-
     path('contactus_generator/', contactus_generator),
-    path('contactus_list/', contactus_list),
-    path('contactus_create/', contactus_create),
-    path('contactus_details/<int:contactus_id>', contactus_details),
-    path('contactus_update/<int:contactus_id>', contactus_update),
-    path('contactus_delete/<int:contactus_id>', contactus_delete),
+
+    path('source_list/', views.SourceListView.as_view()),
+    path('source_create/', views.SourceCreateView.as_view()),
+    path('source_update/<int:pk>', views.SourceUpdateView.as_view()),
+    path('source_delete/<int:pk>', views.SourceDeleteView.as_view()),
+    path('source_details/<int:pk>', views.SourceDetailsView.as_view()),
+
+    path('rate_list/', views.RateListView.as_view()),
+    path('rate_create/', views.RateCreateView.as_view()),
+    path('rate_update/<int:pk>', views.RateUpdateView.as_view()),
+    path('rate_delete/<int:pk>', views.RateDeleteView.as_view()),
+    path('rate_details/<int:pk>', views.RateDetailsView.as_view()),
+
+    path('contactus_list/', views.ContactUsListView.as_view()),
+    path('contactus_create/', views.ContactUsCreateView.as_view()),
+    path('contactus_update/<int:pk>', views.ContactUsUpdateView.as_view()),
+    path('contactus_delete/<int:pk>', views.ContactUsDeleteView.as_view()),
+    path('contactus_details/<int:pk>', views.ContactUsDetailsView.as_view()),
+
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
