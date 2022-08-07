a6bda79742f425738b197d71385ecf1c5983a576
Andrii Bobrov <aspyrin77@gmail.com>
2022-08-08 02:36:21 +0300

    create model ResponseLog

--- app/currency/models.py
+++ app/currency/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from currency.model_choices import CarrencyType
 
 
+class ResponseLog(models.Model):
+    response_time = models.FloatField()
+    request_method = models.CharField(max_length=10)
+    path = models.CharField(max_length=200)
+    query_params = models.CharField(max_length=500)
+    ip = models.CharField(max_length=20)
+
+
 class Source(models.Model):
     source_url = models.CharField(max_length=255)
     name = models.CharField(max_length=64)
